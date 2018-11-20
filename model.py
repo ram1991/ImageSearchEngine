d2c9d21b5122c55026f3356a56e2fc9c90d48f4e
Mouli Uddanti <uddanti.mouli91@gmail.com>
2018-11-20 18:19:59 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,17 +7,10 @@ from keras.applications import imagenet_utils
 from keras.applications.inception_v3 import preprocess_input
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
-import numpy as np
-import argparse
-import cv2
-
-
 from keras.preprocessing import image
 from keras.models import Model
 from keras.models import model_from_json
-
 from keras.layers import Input
-
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import glob
@@ -27,3 +20,15 @@ import os
 import json
 import datetime
 import time
+
+
+model = ResNet50(weights = 'imagenet', include_top = False, pooling = 'avg')
+
+image_path = 'image.jpg'
+
+image = load_img(image_path, target_size = (224,224))
+image = image.img_to_array(image)
+image = np.expand_dims(image, axis = 0)
+image = preprocess_input(image)
+
+predict = model.predict(image)
